2f0889c98e8c8a7fcbb4172459268a8a6a3b06d5
j38472 <34500603+j38472@users.noreply.github.com>
2021-04-26 17:29:39 +0800

    入口
    没啥好说的

--- 中国供应商/run/__init__.py
+++ 中国供应商/run/__init__.py
@@ -1,8 +1,12 @@
+import datetime
+
 import requests
 from fake_useragent import UserAgent
 from urllib.parse import quote
 from urllib.parse import unquote
 import craw
+from requests.adapters import HTTPAdapter
+
 '''=================================================
 @Project -> File   ：qiyangPythonCrawler
 @IDE    ：PyCharm
@@ -11,18 +15,30 @@ import craw
 @Desc   ：启动项  入口处
 ================================================='''
 if __name__ == '__main__':
-    ua = UserAgent()
-    headers = {'User-Agent': ua.random}
-    # "https://www.china.cn/chuishishebei/4152876380.html"
-    response = requests.get(url="url", headers=headers).text
+    Headers = {
+    'content-type': 'application/json', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36'
+    }
 
+    url = "https://www.google.com.hk"
 
-    # 获取电话号
-    craw.start();
-
-
-    print(unquote("%E9%A5%AE%E6%96%99"))
-    ua = UserAgent()
+    keep = True
+    maxtimes = 3
+    count = 0
+    print(datetime.datetime.now())
+    while keep and count < maxtimes:
+        try:
+            res = requests.head(url=url, headers=Headers, timeout=5)
+            keep = False
+        except Exception as e:
+            print(datetime.datetime.now())
+            count = count + 1
+            print('重试' + str(count))
+    # # 获取电话号
+    # craw.start();
+    #
+    #
+    # print(unquote("%E9%A5%AE%E6%96%99"))
+    # ua = UserAgent()
     # print(ua.ie)
     # print(ua.opera)
     # print(ua.chrome)
