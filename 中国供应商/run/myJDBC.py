06318ef1f0c67b188aae4139876ee13fa02aa792
j38472 <34500603+j38472@users.noreply.github.com>
2021-04-26 17:28:08 +0800

    对数据库的各种操作

--- 中国供应商/run/myJDBC.py
+++ 中国供应商/run/myJDBC.py
@@ -20,39 +20,90 @@ conn = pymysql.connect(
 cursor = conn.cursor()
 
 """
+获取tableName 表 的所有
+并形成二维数组 返回
+"""
+
+
+def getDataDb(tableName):
+    sql = """SELECT * FROM %s;
+                        """ % (tableName)
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print(len(results))
+    # cont = 0
+    # for l in results:
+    #     cont+=1
+    #     print("第",cont,"个数据:: ",l)
+    return results
+
+
+
+"""
+给我一个list 和表名  让我存入分类字段表(relatedwords)里
+"""
+
+
+def insGJ(dataList,titDb):
+    for data in dataList:
+        sql = """
+        INSERT INTO %s (url) VALUES('%s')
+                    """ % (titDb,data)
+        cursor.execute(sql)
+        conn.commit()
+
+
+"""
 插入数据   如果没有那个数据   也应该设置为空
 """
+
+
 def insDataAll(data):
     # try:
-        name = ""
-        url = ""
-        zy = ""
-        lxr = ""
-        dh = ""
-        sj = ""
-        dz = ""
+    name = ""
+    url = ""
+    zy = ""
+    lxr = ""
+    dh = ""
+    sj = ""
+    dz = ""
 
-        if data[0] != None:
-            name = data[0]
-        if data[1] != None:
-            url = data[1]
-        if data[2] != None:
-            c = data[2]
-        print(name)
-        print(url)
-        print(c)
-        sql = """
+    if data[0] != None:
+        name = data[0]
+    if data[1] != None:
+        url = data[1]
+    if data[2] != None:
+        c = data[2]
+    print(name)
+    print(url)
+    print(c)
+    sql = """
             INSERT INTO ziyouname (Name,Url) VALUES('测试3%s','%s')
             """ % (name, url)
-        # cursor.execute(sql)
-        # conn.commit()
+    # cursor.execute(sql)
+    # conn.commit()
 
-    # except:
-        # 发生错误就回滚数据
-        # conn.rollback()
+"""
+修改isOrNo为1 表示其已经处理过了
+"""
+def updateIsOrNo(tableDb,id):
+    sql = """
+    UPDATE %s SET isOrNo=1 WHERE id = %s
+    """%(tableDb,id)
+    # print(sql)
+    cursor.execute(sql)
+    conn.commit()
+    return None
+
+# except:
+# 发生错误就回滚数据
+# conn.rollback()
 
 
 if __name__ == '__main__':
-    cursor.execute("SELECT Name,Url FROM ziyouname WHERE id<50;")
-    for data in cursor.fetchall():
-        insDataAll(data)
+    getDataDb("relatedwords")
+    # cursor.execute("SELECT Name,Url FROM ziyouname WHERE id<50;")
+    # for data in cursor.fetchall():
+    #     insDataAll(data)
+
+
