1b1eed71482a1189270bc0f17d3949369f3a3d7e
j38472 <34500603+j38472@users.noreply.github.com>
2021-04-26 17:27:52 +0800

    测试用的

--- 中国供应商/run/test.py
+++ 中国供应商/run/test.py
@@ -7,11 +7,9 @@
 @Date   ：2021/4/26 10:08
 @Desc   ：
 =================================================='''
+
 if __name__ == '__main__':
-    sql = """
-               DELETE FROM %s WHERE Url IN
-                (SELECT Url FROM (SELECT Url FROM %s GROUP BY Url HAVING COUNT(Url)>1) e)
-                AND Id NOT IN 
-                (SELECT Id FROM (SELECT MIN(Id) AS Id FROM %s GROUP BY Url HAVING COUNT(Url)>1) t);
-               """ % ("table_name", "table_name", "table_name")
-    print(sql)
+    s = 'nihaao,shijie'
+    t = 'nihao,'
+    res = s.find(t)
+    print(res)
